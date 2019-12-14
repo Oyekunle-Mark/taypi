b9e30366eae2ecc394bf3c7b96a2d432bd70b807
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-14 11:25:04 +0100

    log chains hello func and performs its own operation

---
+++ gwp3/chain_handlerfunc/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"runtime"
+)
+
+func helloFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hola!")
+}
+
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Printf("Hanlder function called: %s", name)
+
+		h(w, r)
+	}
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:5000",
+	}
+
+	http.HandleFunc("/hello", log(helloFunc))
+
+	server.ListenAndServe()
+}
