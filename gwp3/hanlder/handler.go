b8fe103b9878f9c4eddb25b465bc65b257100108
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-14 11:05:57 +0100

    Refactored for two handlers

--- gwp3/hanlder/handler.go
+++ gwp3/hanlder/handler.go
@@ -8,16 +8,25 @@ import (
 type hello struct{}
 
 func (h *hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+	fmt.Fprint(w, "Hello!")
+}
+
+type world struct{}
+
+func (h *world) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "World!")
 }
 
 func main() {
-	handler := hello{}
+	hello := hello{}
+	world := world{}
 
 	server := http.Server{
-		Addr:    "127.0.0.1:5000",
-		Handler: &handler,
+		Addr: "127.0.0.1:5000",
 	}
+
+	http.Handle("/hello", &hello)
+	http.Handle("world", &world)
 
 	server.ListenAndServe()
 }
