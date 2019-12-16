65c204b42c3a10ef686694d353a8304f5ca604ac
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-16 15:55:34 +0100

    Shows the value of the cookie received

--- gwp4/flash_message/flash.go
+++ gwp4/flash_message/flash.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/base64"
+	"fmt"
 	"net/http"
+	"time"
 )
 
 func setMessage(w http.ResponseWriter, r *http.Request) {
@@ -16,6 +18,34 @@ func setMessage(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &cookie)
 }
 
-func main() {
+func showMessage(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("flash")
 
+	if err != nil {
+		if err == http.ErrNoCookie {
+			fmt.Fprintln(w, "No message found")
+		}
+	} else {
+		newCookie := http.Cookie{
+			Name:    "flash",
+			MaxAge:  -1,
+			Expires: time.Unix(1, 0),
+		}
+
+		http.SetCookie(w, &newCookie)
+
+		decodedMsg, _ := base64.URLEncoding.DecodeString(cookie.Value)
+		fmt.Fprintln(w, string(decodedMsg))
+	}
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:5000",
+	}
+
+	http.HandleFunc("/set", setMessage)
+	http.HandleFunc("/show", showMessage)
+
+	server.ListenAndServe()
 }
