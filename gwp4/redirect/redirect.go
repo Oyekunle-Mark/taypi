f38bd5bb70d121858b557ccfe1e1581c97301b7a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-16 14:00:36 +0100

    Wrote the redirect to the response header

---
+++ gwp4/redirect/redirect.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<title>Go Web Programming</title>
+		</head>
+		<body>
+			<h1>Hello World!</h1>
+		</body>
+		</html>
+	`
+
+	w.Write([]byte(str))
+}
+
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+
+	fmt.Fprintln(w, "Service not available.")
+}
+
+func redirectExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http:www.google.com")
+	w.WriteHeader(302)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:5000",
+	}
+
+	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeHeader", writeHeaderExample)
+	http.HandleFunc("/redirect", redirectExample)
+
+	server.ListenAndServe()
+}
