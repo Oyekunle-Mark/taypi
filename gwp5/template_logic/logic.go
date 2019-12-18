01ceb1ae2b50a7cbcd4451f1588b34d98fc65022
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-18 11:58:21 +0100

    Added the basic logic

---
+++ gwp5/template_logic/logic.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("simple.html")
+	t.Execute(w, "Very basic template!")
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:5000",
+	}
+
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
