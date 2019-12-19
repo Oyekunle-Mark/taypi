55d3d7c149eccc062912f9250e29f40af07cfc1d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-19 16:41:29 +0100

    Added a new table

--- gwp6/database_crud/setup.sql
+++ gwp6/database_crud/setup.sql
@@ -1,6 +1,20 @@
+DROP TABLE posts
+CASCADE
+IF EXISTS;
+DROP TABLE comments
+IF EXISTS;
+
 CREATE TABLE posts
 (
     id SERIAL PRIMARY KEY,
     content TEXT,
     author VARCHAR(255)
+)
+
+CREATE TABLE comments
+(
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    author VARCHAR(255),
+    post_id INTEGER REFERENCES posts(id)
 )
