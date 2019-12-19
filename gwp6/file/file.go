0fecf67585945b2f3664957813b07d375af9346a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-19 14:59:29 +0100

    Reads and writes to a file

--- gwp6/file/file.go
+++ gwp6/file/file.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
@@ -16,4 +17,19 @@ func main() {
 
 	readData, _ := ioutil.ReadFile("data1")
 	fmt.Println("Read file data1:\n", string(readData))
+
+	file1, _ := os.Create("data2")
+	defer file1.Close()
+
+	bytes, _ := file1.Write(data)
+	fmt.Printf("Wrote %d bytes to file1\n", bytes)
+
+	file2, _ := os.Open("data2")
+	defer file2.Close()
+
+	readData2 := make([]byte, len(data))
+	bytes, _ = file2.Read(readData2)
+
+	fmt.Printf("Read %d bytes from file\n", bytes)
+	fmt.Println(string(readData2))
 }
