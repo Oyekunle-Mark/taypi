d593a96d24b26265e704b906f8243969997172ac
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-19 15:10:10 +0100

    Write data in memory to the csv file

--- gwp6/csv/csv.go
+++ gwp6/csv/csv.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+)
+
 type Post struct {
 	ID      int
 	Content string
@@ -7,5 +13,30 @@ type Post struct {
 }
 
 func main() {
+	csvFile, err := os.Create("posts.csv")
 
+	if err != nil {
+		panic(err)
+	}
+
+	defer csvFile.Close()
+
+	allPosts := []Post{
+		Post{1, "Mickey rocks", "Walt Disnery"},
+		Post{2, "Tom is after Jerry", "Warner Bros."},
+		Post{3, "Who run the world?", "Sharks"},
+	}
+
+	writer := csv.NewWriter(csvFile)
+
+	for _, post := range allPosts {
+		line := []string{strconv.Itoa(post.ID), post.Content, post.Author}
+		err := writer.Write(line)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	writer.Flush()
 }
