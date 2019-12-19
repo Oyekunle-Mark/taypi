65147487ddc53853c6c1d0b72afdb7f37e99f553
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-19 15:46:04 +0100

    Db connection added to the init function

--- gwp6/database_crud/crud.go
+++ gwp6/database_crud/crud.go
@@ -9,3 +9,13 @@ type Post struct {
 }
 
 var Db *sql.DB
+
+func init() {
+	var err error
+
+	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+
+	if err != nil {
+		panic(err)
+	}
+}
