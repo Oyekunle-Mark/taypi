5b6a38908d9f51d7ca9dc33932da7006e9900a3e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-19 14:50:07 +0100

    Reads and writes to file using the ioutil library

---
+++ gwp6/file/file.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	data := []byte("Hello World!\n")
+
+	err := ioutil.WriteFile("data1", data, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
+	readData, _ := ioutil.ReadFile("data1")
+	fmt.Println("Read file data1", readData)
+}
