676e3362a101972f7aecd3520c4539092a2db224
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 11:45:22 +0100

    Copied from the process.go file

---
+++ gwp7/normal_xml/xml.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// Post is the structure of the XML file
+type Post struct {
+	XMLName xml.Name `xml:"post"`
+	ID      int      `xml:"id,attr"`
+	Content string   `xml:"content"`
+	Author  Author   `xml:"author"`
+	XML     string   `xml:",innerxml"`
+}
+
+// Author is the author element
+type Author struct {
+	ID   int    `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
+
+func main() {
+	xmlFile, err := os.Open("post.xml")
+
+	if err != nil {
+		fmt.Println("Error opening XML file:", err)
+		return
+	}
+
+	defer xmlFile.Close()
+
+	xmlData, err := ioutil.ReadAll(xmlFile)
+
+	if err != nil {
+		fmt.Println("Error reading XML file:", err)
+		return
+	}
+
+	var post Post
+
+	xml.Unmarshal(xmlData, &post)
+	fmt.Println(post)
+}
