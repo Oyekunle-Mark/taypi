01279baa2dfc6d1c2e120f253caf1c8b1d0afb67
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 11:48:41 +0100

    Updated the type

--- gwp7/normal_xml/xml.go
+++ gwp7/normal_xml/xml.go
@@ -9,17 +9,25 @@ import (
 
 // Post is the structure of the XML file
 type Post struct {
-	XMLName xml.Name `xml:"post"`
-	ID      int      `xml:"id,attr"`
-	Content string   `xml:"content"`
-	Author  Author   `xml:"author"`
-	XML     string   `xml:",innerxml"`
+	XMLName  xml.Name  `xml:"post"`
+	ID       int       `xml:"id,attr"`
+	Content  string    `xml:"content"`
+	Author   Author    `xml:"author"`
+	Comments []Comment `xml:"comments>comment"`
+	XML      string    `xml:",innerxml"`
 }
 
 // Author is the author element
 type Author struct {
 	ID   int    `xml:"id,attr"`
 	Name string `xml:",chardata"`
+}
+
+// Comment is the comment element
+type Comment struct {
+	ID      int    `xml:"id,attr"`
+	Content string `xml:"content"`
+	Author  Author `xml:"author"`
 }
 
 func main() {
