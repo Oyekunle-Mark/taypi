2bcec29482247b03c11cb1e4d3849e7e54ee5524
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 12:13:43 +0100

    Writes the marshalled struct to file

--- gwp7/marshall_xml/xml.go
+++ gwp7/marshall_xml/xml.go
@@ -1,15 +1,43 @@
 package main
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+)
 
 type Post struct {
 	XMLName xml.Name `xml:"post"`
-	ID      int      `xml:"id,attr"`
+	ID      string   `xml:"id,attr"`
 	Content string   `xml:"content"`
 	Author  Author   `xml:"author"`
 }
 
 type Author struct {
-	ID   int    `xml:"id,attr"`
+	ID   string `xml:"id,attr"`
 	Name string `xml:",chardata"`
+}
+
+func main() {
+	post := Post{
+		ID:      "1",
+		Content: " Hello World!", Author: Author{
+			ID:   "2",
+			Name: "Sau Sheong",
+		},
+	}
+
+	xmlData, err := xml.Marshal(&post)
+
+	if err != nil {
+		fmt.Println("Error marshalling to xml:", err)
+		return
+	}
+
+	err = ioutil.WriteFile("post.xml", xmlData, 0644)
+
+	if err != nil {
+		fmt.Println("Error writing xml to file:", err)
+		return
+	}
 }
