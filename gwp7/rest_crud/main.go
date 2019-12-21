ba8eeafb82f697b38a8639e8e04d16abc9be792e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 15:56:20 +0100

    Handles put request

--- gwp7/rest_crud/main.go
+++ gwp7/rest_crud/main.go
@@ -58,3 +58,31 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 	w.WriteHeader(200)
 	return
 }
+
+func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+
+	if err != nil {
+		return
+	}
+
+	post, err := data.Retrieve(id)
+
+	if err != nil {
+		return
+	}
+
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &post)
+	err = post.Update()
+
+	if err != nil {
+		return
+	}
+
+	w.WriteHeader(200)
+	return
+}
