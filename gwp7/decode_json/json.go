c80187ff06b8a83aaa384448d3868d38f4a0a4ad
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 13:08:14 +0100

    Added boilerplate code

---
+++ gwp7/decode_json/json.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// Post is the structure of the json file
+type Post struct {
+	ID       int       `json:"id"`
+	Content  string    `json:"content"`
+	Author   Author    `json:"author"`
+	Comments []Comment `json:"comments"`
+}
+
+// Author is the author object
+type Author struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// Comment is the comment object
+type Comment struct {
+	ID      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func main() {
+	jsonFile, err := os.Open("post.json")
+
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer jsonFile.Close()
+
+	jsonData, err := ioutil.ReadAll(jsonFile)
+
+	if err != nil {
+		fmt.Println("Error reading json data:", err)
+		return
+	}
+
+	var post Post
+
+	json.Unmarshal(jsonData, &post)
+	fmt.Println(post)
+}
