a605f1995dda23c6b1ff935f2fe83aaeb1e62aa4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 15:28:06 +0100

    Removed the unused comment struct

--- gwp7/rest_crud/data.go
+++ gwp7/rest_crud/data.go
@@ -11,15 +11,6 @@ type Post struct {
 	ID       int
 	Content  string
 	Author   string
-	Comments []Comment
-}
-
-// Comment added to posts
-type Comment struct {
-	ID      int
-	Content string
-	Author  string
-	Post    *Post
 }
 
 // Db instance
