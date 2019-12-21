8c05361a5d883ca9b8888c0ec603ffbb8096cccf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 13:28:28 +0100

    Created the data using struct

--- gwp7/marshall_json/json.go
+++ gwp7/marshall_json/json.go
@@ -1,3 +1,5 @@
+package main
+
 // Post is the structure of the json file
 type Post struct {
 	ID       int       `json:"id"`
@@ -17,4 +19,27 @@ type Comment struct {
 	ID      int    `json:"id"`
 	Content string `json:"content"`
 	Author  string `json:"author"`
+}
+
+func main() {
+	post := Post{
+		ID:      1,
+		Content: "Hello World!",
+		Author: Author{
+			ID:   2,
+			Name: "Sau Sheong",
+		},
+		Comments: []Comment{
+			Comment{
+				ID:      3,
+				Content: "Have a great day!",
+				Author:  "Adam",
+			},
+			Comment{
+				ID:      4,
+				Content: "How are you today?",
+				Author:  "Betty",
+			},
+		},
+	}
 }
