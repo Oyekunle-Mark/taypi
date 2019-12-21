cff215824e95104c8376ddc5ff5d95013887e5fe
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 12:07:41 +0100

    The struct to server as the xml template

---
+++ gwp7/marshall_xml/xml.go
@@ -0,0 +1,15 @@
+package main
+
+import "encoding/xml"
+
+type Post struct {
+	XMLName xml.Name `xml:"post"`
+	ID      int      `xml:"id,attr"`
+	Content string   `xml:"content"`
+	Author  Author   `xml:"author"`
+}
+
+type Author struct {
+	ID   int    `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
