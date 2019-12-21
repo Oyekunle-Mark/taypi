60dc2e4209e8c7dc28bd0487ee7c80338d1eba4f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-21 13:06:12 +0100

    Commented exported types

--- gwp7/parse_json/json.go
+++ gwp7/parse_json/json.go
@@ -7,6 +7,7 @@ import (
 	"os"
 )
 
+// Post is the structure of the json file
 type Post struct {
 	ID       int       `json:"id"`
 	Content  string    `json:"content"`
@@ -14,11 +15,13 @@ type Post struct {
 	Comments []Comment `json:"comments"`
 }
 
+// Author is the author object
 type Author struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
 
+// Comment is the comment object
 type Comment struct {
 	ID      int    `json:"id"`
 	Content string `json:"content"`
