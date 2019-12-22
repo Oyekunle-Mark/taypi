353ae7cb7e23662565ba00900609442b516cd0e4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-22 20:18:23 +0100

    Changed the default formatter

--- gwp7/rest_crud/data/setup.sql
+++ gwp7/rest_crud/data/setup.sql
@@ -1,20 +1,16 @@
 -- drop the tables first should they exist
 DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS comments;
-
 -- Create posts table
-CREATE TABLE posts
-(
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    author VARCHAR(255)
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  content TEXT,
+  author VARCHAR(255)
 );
-
 -- Create comments table
-CREATE TABLE comments
-(
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    author VARCHAR(255),
-    post_id INTEGER REFERENCES posts(id)
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  content TEXT,
+  author VARCHAR(255),
+  post_id INTEGER REFERENCES posts(id)
 );
