14ee060ac2abca72d474805a039727b45dea53e3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-22 19:46:36 +0100

    Tests the handleGet function

---
+++ gwp7/rest_crud/request/request_test.go
@@ -0,0 +1,30 @@
+package request
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Oyekunle-Mark/taypi/gwp7/rest_crud/data"
+)
+
+func TestHandleGet(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/post/", HandleRequest)
+
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v, expected 200.", writer.Code)
+	}
+
+	var post data.Post
+	json.Unmarshal(writer.Body.Bytes(), &post)
+
+	if post.ID != 1 {
+		t.Error("Cannot retrieve JSON post")
+	}
+}
