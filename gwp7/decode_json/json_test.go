154400337a17fe10fb0ef2797f269d7ed70c6a4c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-22 18:46:03 +0100

    Removed hard coded values

--- gwp7/decode_json/json_test.go
+++ gwp7/decode_json/json_test.go
@@ -9,11 +9,14 @@ func TestDecode(t *testing.T) {
 		t.Error(err)
 	}
 
-	if post.ID != 1 {
-		t.Errorf("Wrong ID. Got %d, was expecting %d", post.ID, 1)
+	id := 1
+
+	if post.ID != id {
+		t.Errorf("Wrong ID. Got %d, was expecting %d", post.ID, id)
 	}
 
 	content := "Hello World!"
+
 	if post.Content != content {
 		t.Errorf("Wrong post content. Got %s, was expecting %s", post.Content, content)
 	}
